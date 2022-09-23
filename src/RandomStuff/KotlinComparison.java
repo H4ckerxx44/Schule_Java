8f91df5a82a4203a49ade4674079b3d68a220912
H4ckerxx44 <dennis.wortmann1999@gmail.com>
2022-09-23 19:05:17 +0200

    feat: kotlin compare

---
+++ src/RandomStuff/KotlinComparison.java
@@ -0,0 +1,15 @@
+package RandomStuff;
+
+public class KotlinComparison
+{
+	public static void main(String[] args)
+	{
+		double result = area(5.0, 5.0);
+		System.out.println(result);
+	}
+
+	public static double area(double a, double b)
+	{
+		return a * b;
+	}
+}
---
+++ src/RandomStuff/KotlinComparison.kt
@@ -0,0 +1,12 @@
+package RandomStuff
+
+fun main()
+{
+	val result = area(5.0 ,5.0)
+	println(result)
+}
+
+fun area(a: Double, b: Double): Double
+{
+	return a * b
+}
