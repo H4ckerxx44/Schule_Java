cc65077823ecb1f3df86e3d97f3b595032dfb785
H4ckerxx44 <dennis.wortmann1999@gmail.com>
2022-10-05 18:13:59 +0200

    refactor: use int

--- src/Problemstellungen/Bitverschiebung.java
+++ src/Problemstellungen/Bitverschiebung.java
@@ -1,20 +1,18 @@
 package Problemstellungen;
 
-import java.math.BigDecimal;
-
 public class Bitverschiebung
 {
 	public static void main(String[] args)
 	{
-		moveBits(2, 64);
+		moveBits(1, 32);
 	}
 
 	public static void moveBits(int to_move, int limit)
 	{
-		for (int i = 0; i < limit; i++)
+		for (int i = 1; i < limit + 1; i++)
 		{
-			BigDecimal moved = BigDecimal.valueOf((long) to_move << i);
-			System.out.printf("%d << %d = %,.0f\n", to_move, i, moved);
+			int moved =  to_move << i;
+			System.out.printf("%d << %d = %,d\n", to_move, i, moved);
 		}
 	}
 }
